12268938d389cb55c436f7c2f8275d187afa7731
Mohit Patni <mohitpatni0731@gmail.com>
2022-10-01 21:52:56 +0530

    Create swaptwovariable.py

---
+++ swaptwovariable.py
@@ -0,0 +1,7 @@
+P = int( input("Please enter value for P: "))  
+Q = int( input("Please enter value for Q: "))  
+temp_1 = P  
+P = Q  
+Q = temp_1  
+print ("The Value of P after swapping: ", P)  
+print ("The Value of Q after swapping: ", Q)  
